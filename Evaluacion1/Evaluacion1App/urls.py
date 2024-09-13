9d96846a57cedf2ce0ef930e2f8a0f8b3a774fe4
Reeddox <oscaracevedosanchez@gmail.com>
2024-09-13 12:17:36 -0300

    imagenes
    se logro meter links en las imagenes

--- Evaluacion1/Evaluacion1App/urls.py
+++ Evaluacion1/Evaluacion1App/urls.py
@@ -4,8 +4,8 @@ from django.urls import path, include
 from Evaluacion1App import views
 
 urlpatterns = [
-    path('Armas/', views.Armas),
-    path('Espada/', views.Espada),
-    path('Escudo/', views.Escudo),
-    path('Arco/', views.Arco),
+    path('Peliculas/', views.Peliculas),
+    path('Peliculas/Batman1/', views.Batman1),
+    path('Peliculas/Batman2/', views.Batman2),
+    path('Peliculas/Batman3/', views.Batman3),
 ]
--- Evaluacion1/Evaluacion1App/views.py
+++ Evaluacion1/Evaluacion1App/views.py
@@ -2,27 +2,29 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def Armas(request):
+def Peliculas(request):
     return render(request, 'evaluacion1App/index.html')
 
-def Espada(request):
+def Batman1(request):
     data = {
-        "nombre" : "Espada",
-        "desc" : "Esta muy afilada",
-        "imagen" : "espada.png"
+        "nombre" : "Mascara del fantasma",
+        "desc" : "Muy buena pelicula",
+        "imagen" : "batman1.jpg"
     }
     return render(request, 'evaluacion1App/pagina1.html', data)
-def Escudo(request):
+
+def Batman2(request):
     data = {
-        "nombre" : "Escudo",
-        "desc" : "Me protege muy bien",
-        "imagen" : "escudo.png"
+        "nombre" : "Gaslight",
+        "desc" : "Mala pelicula",
+        "imagen" : "batman2.jfif"
     }
     return render(request, 'evaluacion1App/pagina1.html', data)
-def Arco(request):
+
+def Batman3(request):
     data = {
-        "nombre" : "Arco",
-        "desc" : "Tengo buena punteria",
-        "imagen" : "arco.png"
+        "nombre" : "Harley Quinn",
+        "desc" : "Decente pelicula",
+        "imagen" : "batman3.jpg"
     }
     return render(request, 'evaluacion1App/pagina1.html', data)
